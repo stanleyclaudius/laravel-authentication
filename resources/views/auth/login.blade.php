ba1b4f8052e42e5c6a02f14f13f83eab1c561f77
Stanley Claudius <stanleyclaudiuss@gmail.com>
2020-08-06 21:43:52 +0700

    Create login page and main page for templating

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,11 @@
+@extends('template/main')
+
+@section('title', 'Laravel Auth | Login Page')
+
+@section('content')
+<div class="row">
+    <div class="col-md-6">
+        <h2>Login</h2>
+    </div>
+</div>
+@endsection
